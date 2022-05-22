c7547b96b159a1e57b7735ce9898eb397d380bae
Rod Farry <86787226+RodFarry@users.noreply.github.com>
2022-05-22 14:02:53 +0100

    Add audio to player on move

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,13 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rotationThrust = 1f;
     Rigidbody rb;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,10 +26,17 @@ public class Movement : MonoBehaviour
 
     void ProcessThrust()
     {
-
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 
@@ -42,10 +52,10 @@ public class Movement : MonoBehaviour
         }
     }
 
-    public void ApplyRotation(float rotationThisFrame)
+    void ApplyRotation(float rotationThisFrame)
     {
-        rb.freezeRotation = true; // Freezing rotation so we can manually rotate
+        rb.freezeRotation = true;  // freezing rotation so we can manually rotate
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
-        rb.freezeRotation = false; // Unfreezing rotation so physics system can take over
+        rb.freezeRotation = false;  // unfreezing rotation so the physics system can take over
     }
 }
